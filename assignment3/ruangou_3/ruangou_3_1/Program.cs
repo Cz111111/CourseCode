fe935a9925503b8fb0a422379814739576b2e0a0
ChenZH <940527909@qq.com>
2024-03-13 16:00:01 +0800

    第三次作业

---
+++ assignment3/ruangou_3/ruangou_3_1/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ruangou_3_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IShape rectangle = new Rectangle(5.0, 3.0);
+            IShape square = new Square(4.0);
+            IShape triangle = new Triangle(6.0, 8.0);
+
+            Console.WriteLine($"Rectangle: Area = {rectangle.CalculateArea()}, IsValid = {rectangle.IsValidShape()}");
+            Console.WriteLine($"Square: Area = {square.CalculateArea()}, IsValid = {square.IsValidShape()}");
+            Console.WriteLine($"Triangle: Area = {triangle.CalculateArea()}, IsValid = {triangle.IsValidShape()}");
+        }
+    }
+    interface IShape
+    {
+        double CalculateArea();
+        bool IsValidShape();
+    }
+    abstract class Shape : IShape
+    {
+        public string Name { get; set; }
+
+        public abstract double CalculateArea();
+
+        public abstract bool IsValidShape();
+    }
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public override bool IsValidShape()
+        {
+            return length > 0 && width > 0;
+        }
+    }
+
+    class Square : Shape
+    {
+        private double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public override double CalculateArea()
+        {
+            return side * side;
+        }
+
+        public override bool IsValidShape()
+        {
+            return side > 0;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private double length;
+        private double height;
+
+        public Triangle(double length, double height)
+        {
+            this.length = length;
+            this.height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * length * height;
+        }
+
+        public override bool IsValidShape()
+        {
+            return length > 0 && height > 0;
+        }
+    }
+}
---
+++ assignment3/ruangou_3/ruangou_3_2/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ruangou_3_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rand=new Random();
+            IShape[] shapes = new IShape[10];
+            for (int i = 0; i < 10; i++)
+            {
+                shapes[i] = ShapeFactory.CreateShape(rand);
+            }
+
+            double totalArea = 0.0;
+            for (int i = 0; i < 10; i++)
+            {
+                totalArea += shapes[i].CalculateArea();
+                Console.WriteLine($"第{i+1}形状为：{shapes[i].JudgeShape(shapes[i])}");
+            }
+
+            Console.WriteLine($"Total area: {totalArea:0.00}(结果保留两位小数)");
+        }
+    }
+    interface IShape
+    {
+        double CalculateArea();
+        string JudgeShape(IShape shape);
+    }
+
+    abstract class Shape : IShape
+    {
+        public abstract double CalculateArea();
+        public string JudgeShape(IShape shape) {
+            if (shape is Rectangle) { 
+                return "长方形"; 
+            }else if(shape is Square) {
+                return "正方形"; 
+            }else if (shape is Triangle) {
+                return "三角形"; 
+            }else {
+                return null; 
+            }
+
+        }
+    }
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+    }
+
+    class Square : Shape
+    {
+        private double side;
+
+        public Square(double side)
+        {
+            this.side = side;
+        }
+
+        public override double CalculateArea()
+        {
+            return side * side;
+        }
+    }
+
+    class Triangle : Shape
+    {
+        private double length;
+        private double height;
+
+        public Triangle(double length, double height)
+        {
+            this.length = length;
+            this.height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return 0.5 * length * height;
+        }
+    }
+    class ShapeFactory
+    {
+        public static IShape CreateShape(Random rand)
+        {
+            int shapeType = rand.Next(3);  // 生成 0~2 的随机数，表示形状类型
+
+            switch (shapeType)
+            {
+                case 0:
+                    return new Rectangle(rand.NextDouble(), rand.NextDouble());
+                case 1:
+                    return new Square(rand.NextDouble());
+                case 2:
+                    return new Triangle(rand.NextDouble(), rand.NextDouble());
+                default:
+                    throw new Exception("Invalid shape type");
+            }
+        }
+    }
+}
