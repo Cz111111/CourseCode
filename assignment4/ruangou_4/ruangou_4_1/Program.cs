b0de1150a37c0f0eca8c382c7fd25ffbd2209d8c
ChenZH <940527909@qq.com>
2024-03-20 18:10:11 +0800

    第四次作业

---
+++ assignment4/ruangou_4/ruangou_4_1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ruangou_4_1
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> list = new GenericList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.ForEach(
+                x => Console.Write(x+" "),
+                (a, b) => a > b ? a : b,
+                (a, b) => a < b ? a : b,
+                (a, b) => a+b
+                );
+        }
+    }
+    class Node<T> { 
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t) {
+            Next = null;
+            Data = t;
+        }
+    }
+    class GenericList<T> {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head { 
+            get=> head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail==null)
+            {
+                tail = head = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(
+            Action<T> action, 
+            Func<T, T, T> f_max, 
+            Func<T, T, T> f_min, 
+            Func<T, T, T> f_sum
+            )
+        {
+            Node<T> current = head;
+            T max = head.Data;
+            T min = head.Data;
+            T sum = head.Data;
+            while (current != null)
+            {
+                action(current.Data);
+                max = f_max(max, current.Data);
+                min = f_min(min, current.Data);
+                sum = f_sum(sum, current.Data);
+                current = current.Next;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"最大值: {max}");
+            Console.WriteLine($"最小值: {min}");
+            Console.WriteLine($"求和: {sum}");
+            
+        }
+    }
+    
+}
---
+++ assignment4/ruangou_4/ruangou_4_2/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ruangou_4_2
+{
+    internal class Program
+    {
+        //使用事件机制，模拟实现一个闹钟功能。
+        //闹钟可以有嘀嗒（Tick）事件和响铃（Alarm）两个事件。
+        //在闹钟走时时或者响铃时，在控制台显示提示信息。
+
+        static void Main(string[] args)
+        {
+            DelegateClockEvent clock = new DelegateClockEvent(0,10);
+
+            clock.Tick += Clock_Tick;//注册
+            clock.Alarm += Clock_Alarm;//注册
+
+            for (int i = 0; i < 20; i++)
+            {
+                clock.TickHandler();
+                Console.WriteLine($"当前时间: {clock.CurrentTime}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        // 嘀嗒事件的处理函数
+        static void Clock_Tick()
+        {
+            Console.Write("嘀嗒！ ");
+        }
+        static void Clock_Alarm()
+        {
+            Console.Write("响铃！ ");
+        }
+    }
+    class DelegateClockEvent
+    {
+        public delegate void EventHandler();
+
+        public event EventHandler Tick;
+
+        public event EventHandler Alarm;
+
+        private int time;
+
+        public int CurrentTime { get; set; }
+
+        public DelegateClockEvent(int CurrentTime,int time)
+        {
+            this.CurrentTime = CurrentTime;
+            this.time = time;
+        }
+
+        public void TickHandler()
+        {
+            CurrentTime++;
+            OnTick();
+            if (CurrentTime == time)
+            {
+                OnAlarm();
+            }
+
+        }
+
+        protected virtual void OnTick()
+        {
+            if (Tick != null)
+            {
+                Tick();
+            }
+        }
+
+        protected virtual void OnAlarm()
+        {
+            if (Alarm != null)
+            {
+                Alarm();
+            }
+        }
+    }
+
+}
