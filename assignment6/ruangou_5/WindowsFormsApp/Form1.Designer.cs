1d4472e944041418a4bfd42a164cf9e8696f52cd
ChenZH <940527909@qq.com>
2024-04-10 10:51:55 +0800

    第六次作业

---
+++ assignment6/ruangou_5/WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.showButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.backgroundButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1158, 738);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1152, 658);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 667);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(70, 22);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "拉伸";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.showButton);
+            this.flowLayoutPanel1.Controls.Add(this.clearButton);
+            this.flowLayoutPanel1.Controls.Add(this.backgroundButton);
+            this.flowLayoutPanel1.Controls.Add(this.closeButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(176, 667);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(979, 68);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // showButton
+            // 
+            this.showButton.AutoSize = true;
+            this.showButton.Location = new System.Drawing.Point(876, 3);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(100, 30);
+            this.showButton.TabIndex = 0;
+            this.showButton.Text = "显示图片";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.AutoSize = true;
+            this.clearButton.Location = new System.Drawing.Point(770, 3);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 30);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "清除图片";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // backgroundButton
+            // 
+            this.backgroundButton.AutoSize = true;
+            this.backgroundButton.Location = new System.Drawing.Point(656, 3);
+            this.backgroundButton.Name = "backgroundButton";
+            this.backgroundButton.Size = new System.Drawing.Size(108, 30);
+            this.backgroundButton.TabIndex = 2;
+            this.backgroundButton.Text = "设置背景色";
+            this.backgroundButton.UseVisualStyleBackColor = true;
+            this.backgroundButton.Click += new System.EventHandler(this.backgroundButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.AutoSize = true;
+            this.closeButton.Location = new System.Drawing.Point(550, 3);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(100, 30);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "关闭";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Title = "选择一个图片文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1158, 738);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button backgroundButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ assignment6/ruangou_5/ruangou_5_1/Program.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ruangou_5_1
+{
+    public class MainClass
+    {
+        public static void Main()
+        {
+            try
+            {
+                //顾客
+                Customer customer1 = new Customer(1, "Customer1");
+                Customer customer2 = new Customer(2, "Customer2");
+
+                //商品
+                Goods milk = new Goods(1, "Milk", 69.9f);
+                Goods eggs = new Goods(2, "eggs", 4.99f);
+                Goods apple = new Goods(3, "apple", 5.59f);
+
+                //订单
+                Order order1 = new Order(1, customer1, new DateTime(2021, 3, 21));
+                order1.AddDetails(new OrderDetail(apple, 8));
+                order1.AddDetails(new OrderDetail(eggs, 10));
+                //order1.AddDetails(new OrderDetail(eggs, 8));
+                //order1.AddDetails(new OrderDetail(milk, 10));
+
+                Order order2 = new Order(2, customer2, new DateTime(2021, 3, 21));
+                order2.AddDetails(new OrderDetail(eggs, 10));
+                order2.AddDetails(new OrderDetail(milk, 10));
+
+                Order order3 = new Order(3, customer2, new DateTime(2021, 3, 21));
+                order3.AddDetails(new OrderDetail(milk, 100));
+
+                OrderService orderService = new OrderService();
+                orderService.AddOrder(order1);
+                orderService.AddOrder(order2);
+                orderService.AddOrder(order3);
+
+                Console.WriteLine("\n GetById");
+                Console.WriteLine(orderService.GetById(1));
+                Console.WriteLine(orderService.GetById(5) == null);
+
+                Console.WriteLine("\nGetAllOrders");
+                List<Order> orders = orderService.QueryAll();
+                orders.ForEach(o => Console.WriteLine(o));
+
+                Console.WriteLine("\nGetOrdersByCustomerName:'Customer2'");
+                orders = orderService.QueryByCustomerName("Customer2");
+                orders.ForEach(o => Console.WriteLine(o));
+
+                Console.WriteLine("\nGetOrdersByGoodsName:'eggs'");
+                orders = orderService.QueryByGoodsName("eggs");
+                orders.ForEach(o => Console.WriteLine(o));
+
+                Console.WriteLine("\nGetOrdersTotalAmount:1000");
+                orders = orderService.QueryByTotalPrice(1000);
+                orders.ForEach(o => Console.WriteLine(o));
+
+                Console.WriteLine("\nQueryByCondition");
+                var query = orderService.Query(o => o.TotalPrice > 1000);
+                foreach (Order order in query)
+                {
+                    Console.WriteLine(order);
+                }
+
+                Console.WriteLine("\nRemove order(id=2) and qurey all");
+                orderService.RemoveOrder(2);
+                orderService.QueryAll().ForEach(
+                    o => Console.WriteLine(o));
+
+                Console.WriteLine("\n order by Amount");
+                orderService.Sort(
+                  (o1, o2) => o2.TotalPrice.CompareTo(o1.TotalPrice));
+                orderService.QueryAll().ForEach(
+                    o => Console.WriteLine(o));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+    public class Goods
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        private float price;
+
+        public float Price
+        {
+            get { return price; }
+            set
+            {
+                if (value < 0)
+                    throw new ArgumentException("the price must >= 0!");
+                price = value;
+            }
+        }
+
+        public Goods() { }
+
+        public Goods(int id, string name, float price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var goods = obj as Goods;
+            return goods != null && Id == goods.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Id:{Id}, Name:{Name}, Value:{Price}";
+        }
+
+    }
+    public class Customer
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Customer() { }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"customerId:{Id}, CustomerName:{Name}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var customer = obj as Customer;
+            return customer != null && Id == customer.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + Id.GetHashCode();
+        }
+
+
+    }
+    public class OrderDetail
+    {
+
+        public Goods Goods { get; set; }
+
+        public int Quantity { get; set; }
+
+        public float TotalPrice
+        {
+            get => Goods.Price * Quantity;
+        }
+
+        public OrderDetail() { }
+
+        public OrderDetail(Goods goods, int quantity)
+        {
+            this.Goods = goods;
+            this.Quantity = quantity;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var detail = obj as OrderDetail;
+            return detail != null &&
+                   EqualityComparer<Goods>.Default.Equals(Goods, detail.Goods);
+        }
+
+        public override int GetHashCode()
+        {
+            return 785010553 + EqualityComparer<Goods>.Default.GetHashCode(Goods);
+        }
+
+        public override string ToString()
+        {
+            return $"OrderDetail:{Goods},{Quantity}";
+        }
+    }
+    public class Order : IComparable<Order>
+    {
+
+        private List<OrderDetail> details = new List<OrderDetail>();
+
+        public int Id { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public float TotalPrice
+        {
+            get => Details.Sum(d => d.TotalPrice);
+        }
+
+        public List<OrderDetail> Details => details;
+
+        public Order()
+        {
+            CreateTime = DateTime.Now;
+        }
+
+        public Order(int orderId, Customer customer, DateTime creatTime)
+        {
+            Id = orderId;
+            Customer = customer;
+            CreateTime = creatTime;
+        }
+
+        public void AddDetails(OrderDetail orderDetail)
+        {
+            if (Details.Contains(orderDetail))
+            {
+                throw new ApplicationException($"The goods ({orderDetail.Goods.Name}) exist in order {Id}");
+            }
+            Details.Add(orderDetail);
+        }
+
+        public int CompareTo(Order other)
+        {
+            return (other == null) ? 1 : Id - other.Id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var order = obj as Order;
+            return order != null && Id == order.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public void RemoveDetails(int num)
+        {
+            Details.RemoveAt(num);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append($"orderId:{Id}, customer:({Customer})");
+            Details.ForEach(detail => result.Append("\n\t" + detail));
+            return result.ToString();
+        }
+
+    }
+    public class OrderService
+    {
+
+
+        private List<Order> orders = new List<Order>();
+
+        public OrderService()
+        {
+        }
+
+        //添加订单
+        public void AddOrder(Order order)
+        {
+            if (orders.Contains(order))
+            {
+                throw new ApplicationException($"the order {order.Id} already exists!");
+            }
+            orders.Add(order);
+        }
+
+        //更新订单
+        public void Update(Order order)
+        {
+            int idx = orders.FindIndex(o => o.Id == order.Id);
+            if (idx < 0)
+            {
+                throw new ApplicationException($"the order {order.Id} doesn't exist!");
+            }
+            orders.RemoveAt(idx);
+            orders.Add(order);
+        }
+
+        //根据Id查询订单
+        public Order GetById(int orderId)
+        {
+            return orders.FirstOrDefault(o => o.Id == orderId);
+        }
+
+        //根据Id删除订单
+        public void RemoveOrder(int orderId)
+        {
+            int idx = orders.FindIndex(o => o.Id == orderId);
+            if (idx >= 0)
+            {
+                orders.RemoveAt(idx);
+            }
+            /**
+            orders.RemoveAll(o => o.Id == orderId);
+            */
+        }
+
+        //查询所有订单
+        public List<Order> QueryAll()
+        {
+            return orders;
+        }
+
+        //根据客户名查询
+        public List<Order> QueryByCustomerName(string customerName)
+        {
+            var query = orders
+                .Where(o => o.Customer.Name == customerName)
+                .OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+        //根据货物名查询
+        public List<Order> QueryByGoodsName(string goodsName)
+        {
+            var query = orders.Where(
+              o => o.Details.Any(d => d.Goods.Name == goodsName))
+                .OrderBy(o => o.TotalPrice);
+            return query.ToList();
+
+            /** 其他方法
+            var query2 = orders.Where(
+              o => o.Details.Exists(d => d.Goods.Name == goodsName))
+                .OrderBy(o => o.TotalPrice);
+
+            var query3 = orders.Where(
+             o => o.Details.Where(d => d.Goods.Name == goodsName).Count()>0)
+               .OrderBy(o => o.TotalPrice);
+
+            var query4 = orders.Where(
+             o => HasGoods(o,goodsName)) //写一个HasGoods方法，来检查o中是否包含名为goodName的货物
+               .OrderBy(o => o.TotalPrice); 
+            **/
+        }
+
+        //根据总价查询
+        public List<Order> QueryByTotalPrice(float totalPrice)
+        {
+            var query = orders.Where(o => o.TotalPrice >= totalPrice)
+                .OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+
+        //对orders中的数据进行排序
+        public void Sort(Comparison<Order> comparison)
+        {
+            orders.Sort(comparison);
+        }
+
+        //根据传入的条件进行查询
+        public IEnumerable<Order> Query(Predicate<Order> condition)
+        {
+            return orders.Where(o => condition(o)).OrderBy(o => o.TotalPrice);
+        }
+
+    }
+}
+
+
